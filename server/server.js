173d3fe6ca930050cfbbf0d2f54c6a85599e780d
Dustin Swecker <dustin.swecker@gmail.com>
2023-06-13 13:49:33 -0400

    got server running

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
+const db = require('./config/connection')
 
 const app = express();
 
+db.once('open', () => {
+  console.log('connected to db')
 app.listen(PORT, () => {
   console.log("Server running on PORT 3001!");
+})
 })
