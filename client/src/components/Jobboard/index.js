2864135de4b1d895cffa42b77712039d8469ee48
Dustin Swecker <dustin.swecker@gmail.com>
2023-06-14 12:01:52 -0400

    connected front and backend

--- client/src/components/Jobboard/index.js
+++ client/src/components/Jobboard/index.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import GET_USER from '../../utils/queries'
+import {useState} from 'react';
+import {useQuery, useMutation} from '@apollo/client';
+import { GET_USER } from '../../utils/queries';
+
 
 const JobBoard = () => {
-    return (
-        <div>
-        we connecting
+    const {loading, data} = useQuery(GET_USER);
+    const users = data?.users || [];
+    console.log(data);
+    return (<div>
+        { loading?
+         <div>
+            Loading users....
+        </div>: 
+        <div>             
+        { users[0].username }
+        </div>}
         </div>
-    )
-}
+)}            
+
+    
 
 export default JobBoard;
