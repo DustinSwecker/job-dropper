4e257fc84187fd84c8f90521249ddccf0bff2ec9
Dustin Swecker <dustin.swecker@gmail.com>
2023-06-15 18:57:38 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/components/Skills/skills.css'
 import './App.css';
 import JobBoard from './components/Jobboard';
