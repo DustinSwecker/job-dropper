b2a619c2af4c0a43b2bdf31cab887a933ea53b0a
Meghan Simmons <simmons.meghan@gmail.com>
2023-06-15 15:08:41 -0400

    added APIs for comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,41 +7,24 @@ const resolvers = {
             return await User.find();
         },
 
-        jobs: async () => {
-            return await Job.find();
-        },
-        job: async (parent, { jobId }) => {
-            return await Job.findOne({ _id: jobId });
-        },
-
         singleUser: async(parent, args) => {
             return await User.findOne({ _id: args.userId })
 
         },
+
+        jobs: async () => {
+            return await Job.find();
+        },
+
+        job: async (parent, { jobId }) => {
+            return await Job.findOne({ _id: jobId });
+        },
+
     },
     Mutation: {
         addUser: async(_, args) => {
             const userData = await User.create(args);
             return userData;
-        },
-
-        addJob: async(_, args) => {
-            const jobData = await Job.create(args);
-
-            return jobData;
-        },
-        updateJob: async(_, { jobId, title, description, link, skills, comments }) => {
-             const jobData = await Job.findOneAndUpdate(
-                { _id: jobId }, 
-                {$set:{ title:title, description: description, link:link, skills: skills, comments:comments }},
-                {new: true});
-
-            return jobData;
-         },
-        removeJob: async(_, { jobId }) => {
-            const jobData = await Job.findOneAndDelete({ _id: jobId });
-
-            return jobData;
         },
 
         updateUser: async(_, args) => {
@@ -61,13 +44,55 @@ const resolvers = {
             }
             return updateUserData;
         },
+
         deleteUser: async (_,args) => {
             await User.findOneAndDelete({
                 _id: args.userId
             });
          
             return null
-        }
+        },
+
+        addJob: async(_, args) => {
+            const jobData = await Job.create(args);
+            return jobData;
+        },
+
+        updateJob: async(_, { jobId, title, company, location, description, link, skills }) => {
+             const jobData = await Job.findOneAndUpdate(
+                { _id: jobId }, 
+                {$set: { 
+                    title:title,
+                    company: company,
+                    location: location,
+                    description: description,
+                    link:link, 
+                    skills: skills,
+                }},
+                {new: true});
+            return jobData;
+         },
+
+        removeJob: async(_, { jobId }) => {
+            const jobData = await Job.findOneAndDelete({ _id: jobId });
+            return jobData;
+        },
+        
+        addComment: async(_, { jobId, commentBody, username }) => {
+            const commentData = await Job.findOneAndUpdate(
+               { _id: jobId }, 
+               {$addToSet:{ comments: { commentBody, username } }},
+               {new: true});
+           return commentData;
+        },
+
+        removeComment: async(_, { jobId, commentId }) => {
+            const commentData = await Job.findOneAndUpdate(
+               { _id: jobId }, 
+               {$pull:{ comments: { _id: commentId} }},
+               {new: true});
+           return commentData;
+        },
 
     }
 };
