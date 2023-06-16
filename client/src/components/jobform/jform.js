b18b1eafa45ee74f90dad58665799a0b7b2e6917
ccurrystar9186 <36233138+ccurrystar9186@users.noreply.github.com>
2023-06-16 12:50:57 -0500

    jform

---
+++ client/src/components/jobform/jform.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const JobForm = () => {
+  const [title, setTitle] = useState('');
+  const [company, setCompany] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Process form submission
+    // You can send the form data to an API or perform any necessary actions here
+
+    // Reset form fields
+    setTitle('');
+    setCompany('');
+    setLocation('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="title">Job Title</label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="company">Company</label>
+        <input
+          type="text"
+          className="form-control"
+          id="company"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          className="form-control"
+          id="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  );
+};// comment 
+
+export default JobForm;
