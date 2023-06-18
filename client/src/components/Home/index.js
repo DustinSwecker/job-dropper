3873d58e29375c2364a98e8a75a9195a56ae2ff6
Dustin Swecker <dustin.swecker@gmail.com>
2023-06-17 23:02:43 -0400

    got routing working appropriately and made home page, swapped a few files...some css work to do on forms, but page looks great now

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,77 +1,80 @@
 import React from "react";
-import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Row, Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import image from '../../images/job_dropper_logo.png'
-import { MDBIcon } from "mdb-react-ui-kit";
-import './home.css'
-// import  {Navbar,Nav} from "react-bootstrap";
-// import Login from '../Login';
-// import Auth from '../../utils/auth'
 
-import Auth from '../../utils/auth';
-
-function Home(){
-  const [active, setActive] = useState(false);
-  function addBeat(e) {
-    setActive(true);
-    e.target.children.style = "--fa-beat-scale: 2.0"
-  }
-  function removeBeat(e) {
-    setActive(false);
-  }
-
-    const logout = (event) => {
-        event.preventDefault();
-         Auth.logout();
-      };
-
-
-    return (
-      <div className="containter-fluid d-flex">
-        <div className='d-flex flex-row h-100 justify-content-between'> 
-      {/* Use Link component to create a link that returns to the homepage on click */}
-        <Link  to='/'> 
-        <img id = "logo" src={image}></img>
-        </Link>          
-     </div>
-     
-    
-     {/* <div className="container flex-column justify-space-between-lg "> */}
-                <h2 id = "navmessage"className="mx-2"> Let's Grow Together</h2>
-                <Link to = "/">
-                    <button id="drop" type='button' className='btn btn-info mx-5'> <i onMouseOver = {addBeat}  onMouseOut = {removeBeat} className={active ? "fa-beat fas fa-fill-drip" : "fas fa-fill-drip" }></i> Drop a job </button>
-                </Link>
-                
-
-
-
-          <div className='mx-3 d-flex justify-right' >
+function Home () {
+    return (<div><Container fluid className="m-5">
+    <Card>
+      <Row className="m-5">
+        <Col xs={6} md={8}>
+        <Card.Body>
+        <Card.Title>Why Job Dropper?</Card.Title>
+        <br></br>
+        <Card.Text>
+          Because social networking sites are too formal! They take too long, require too much information! 
+          <br></br>
+          <br></br>
           
+          They're too focused on attracting business interests and not just serving the public and the social needs of those looking for jobs!
+          <br></br>
+          <br></br>
 
-          <div>
-          {Auth.loggedIn() ? (
-            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-              Logout
-            </button>
-          ) : (
-            <>
+          This is a site for people who want to help their friends! For job searchers to see curated lists of jobs shared by buddies!
+          <br></br>
+          <br></br>
 
-              <Link className="btn btn-lg btn-primary m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-primary m-2" to="/signup">
-                Signup
-              </Link>
-           </>
-          )}
-        </div>
+          We want to make job search social in a way it hasn't been done before. So what's stopping you? Go ahead and find a new job!
+        </Card.Text>
+        <Link to = "/listing">
+        <button style={{width: "fit-content"}} type='button' className='btn btn-info'> <i class="fa-solid fa-newspaper"></i> See the Listings! </button>
+        </Link>
+        </Card.Body>
+        </Col>
+        <Col>
+        <Card.Img style={{width: 'fit-content', height: '400px', float: 'left'}} src="https://plus.unsplash.com/premium_photo-1679936310859-bb50897c4c89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGpvYiUyMHNlZWtlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" />
       
-         </div>
-         </div>
-
+      </Col>
+      </Row>
       
+    </Card>
+    </Container>
+    <Container fluid className="m-5">
+    <Card>
+      <Row className="m-5">
+      <Col xs={12}>
+        <Card.Img className="m-5" style={{width: 'fit-content', textAlign: 'center', height: '400px', float: 'left'}} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IK_vt23gFFEvlLLaGvPoyQHaEK%26pid%3DApi&f=1&ipt=eb012a5aa9812de609632193bfce1234586127a8e004748cf7da253da21fd396&ipo=images" />
+      
+      </Col>
+        <Col xs={12} md={12}>
+        <Card.Body>
+        <Card.Title>Found a great job? Awesome! Drop it!</Card.Title>
+        <br></br>
+        <Card.Text>
+          Job posts at Job Dropper are quick and easy! Just fill out the a few quick, easy, peasy form fields and Job Dropper will notify your friends of the sweet new gig you've found!
+          <br></br>
+          <br></br>
+
+          So quit reading and just Drop it!
+          <br></br>
+          <br></br>
+
+          
+        </Card.Text>
+        <Link to = "/pageform">
+        <button style={{width: "fit-content"}} id="drop" type='button' className='btn btn-info'> <i className="fa-beat fas fa-fill-drip" ></i> Drop a job </button>
+        </Link>
+        </Card.Body>
+        </Col>
   
-    );
-};
+      </Row>
+      
+    </Card>
+    </Container>
+    </div>
+    )
+}
+
 export default Home;
