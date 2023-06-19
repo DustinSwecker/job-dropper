3d04e291219aa48531738e9cedf2542224568c01
Meghan Simmons <simmons.meghan@gmail.com>
2023-06-19 18:43:30 -0400

    got skills array checkboxes to post to database and render to page

--- client/src/components/jobform/jform.js
+++ client/src/components/jobform/jform.js
@@ -14,7 +14,7 @@ const JobForm = () => {
     company: '',
     location: '',
     link: '',
-    skills: []
+    skills:[]
   });
 
   const [addJob, { error, data }] = useMutation(ADD_JOB);
@@ -24,19 +24,49 @@ const JobForm = () => {
     setJobInfo ({ ...jobInfo, [name]: value });
   }
 
+  const [skillsArray, setSkillsArray] = useState({
+    skills: []
+  });
+  
+  const handleInputChange2 = (e) => {
+    const { value, checked } = e.target;
+    const { skills } = skillsArray;
+      
+    // console.log(`${value} is ${checked}`);
+     
+    // Case 1 : The user checks the box
+    if (checked) {
+      setSkillsArray({
+        skills: [...skills, value]
+      });
+    }
+  
+    // Case 2  : The user unchecks the box
+    else {
+      setSkillsArray({
+        skills: skills.filter((e) => e !== value)
+      });
+    }
+  }
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const { data } = await addJob({
         variables: {
-          ...jobInfo
+          title: jobInfo.title,
+          description: jobInfo.description,
+          company: jobInfo.company,
+          location: jobInfo.location,
+          link: jobInfo.link,
+          skills: skillsArray.skills
         }
     })
     } catch (err) {
       console.log(err);
     }
   }
-
 
   return (
     <div>
@@ -103,26 +133,26 @@ const JobForm = () => {
       </div>
 
       <div key={`inline-checkbox`} className="mb-3">
-        <Form.Check inline label="Angular" name="skills" type="checkbox" value="Angular" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="AWS" name="skills" type="checkbox" value="AWS" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="Bootstrap" name="skills" type="checkbox" value="Bootstrap" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="C+" name="skills" type="checkbox" value="C+" id={`inline-checkbox-1`} onChange={handleInputChange}/>                           
-        <Form.Check inline label="CSS" name="skills" type="checkbox" value="JavaScript" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="espress.js" name="skills" type="checkbox" value="espress.js" id={`inline-checkbox-1`} onChange={handleInputChange}/>      
-        <Form.Check inline label="github" name="skills" type="checkbox" value="github" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="graphql" name="skills" type="checkbox" value="graphql" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="HTML" name="skills" type="checkbox" value="HTML" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="Java" name="skills" type="checkbox" value="Java" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="JavaScript" name="skills" type="checkbox" value="JavaScript" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="MongoDB" name="skills" type="checkbox" value="MongoDB" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="MYSQL" name="skills" type="checkbox" value="MYSQL" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="node.js" name="skills" type="checkbox" value="node.js" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="python" name="skills" type="checkbox" value="python" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="React" name="skills" type="checkbox" value="React" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="Rust" name="skills" type="checkbox" value="Rust" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="SQL" name="skills" type="checkbox" value="SQL" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="Vue" name="skills" type="checkbox" value="Vue" id={`inline-checkbox-1`} onChange={handleInputChange}/>
-        <Form.Check inline label="Wordpress" name="skills" type="checkbox" value="Wordpress" id={`inline-checkbox-1`} onChange={handleInputChange}/>
+        <Form.Check inline label="Angular" name="skills" type="checkbox" value="Angular" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="AWS" name="skills" type="checkbox" value="AWS" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="Bootstrap" name="skills" type="checkbox" value="Bootstrap" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="C++" name="skills" type="checkbox" value="cplus" id={`inline-checkbox-1`} onChange={handleInputChange2}/>                           
+        <Form.Check inline label="CSS" name="skills" type="checkbox" value="CSS" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="espress.js" name="skills" type="checkbox" value="espress.js" id={`inline-checkbox-1`} onChange={handleInputChange2}/>      
+        <Form.Check inline label="github" name="skills" type="checkbox" value="github" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="graphql" name="skills" type="checkbox" value="graphql" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="HTML" name="skills" type="checkbox" value="HTML" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="Java" name="skills" type="checkbox" value="Java" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="JavaScript" name="skills" type="checkbox" value="JavaScript" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="MongoDB" name="skills" type="checkbox" value="MongoDB" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="MYSQL" name="skills" type="checkbox" value="MYSQL" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="node.js" name="skills" type="checkbox" value="node.js" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="python" name="skills" type="checkbox" value="python" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="React" name="skills" type="checkbox" value="React" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="Rust" name="skills" type="checkbox" value="Rust" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="SQL" name="skills" type="checkbox" value="SQL" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="Vue" name="skills" type="checkbox" value="Vue" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
+        <Form.Check inline label="Wordpress" name="skills" type="checkbox" value="Wordpress" id={`inline-checkbox-1`} onChange={handleInputChange2}/>
   
       </div>
 
